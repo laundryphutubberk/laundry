eb03a13de5f4e149cb0649dd948b4f763ac4a3b7
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 03:02:01 +0700

    BE-01: pass DATABASE_URL to Prisma client

--- backend/src/core/prisma.js
+++ backend/src/core/prisma.js
@@ -1,6 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
+const { env } = require('../config/env');
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: env.DATABASE_URL,
+});
 
 module.exports = {
   prisma,
