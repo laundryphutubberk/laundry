a8652d8fef0fb15622528b8f9ed26d99a7af7a71
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 02:53:35 +0700

    BE-03: include validation details in error meta

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -3,8 +3,9 @@ const { sendFailure } = require('../core/httpResponse');
 const errorMiddleware = (error, _req, res, _next) => {
   const statusCode = error.statusCode || 500;
   const message = statusCode === 500 ? 'Internal server error' : error.message;
+  const meta = error.details ? { details: error.details } : undefined;
 
-  return sendFailure(res, { message }, statusCode);
+  return sendFailure(res, { message }, statusCode, meta);
 };
 
 module.exports = {
