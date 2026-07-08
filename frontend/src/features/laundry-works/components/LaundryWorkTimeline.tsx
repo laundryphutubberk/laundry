7f96cc906119bb665409b77c84443beba19b7677
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 21:12:07 +0700

    FE-01: add laundry work timeline placeholder

---
+++ frontend/src/features/laundry-works/components/LaundryWorkTimeline.tsx
@@ -0,0 +1,9 @@
+/**
+ * Laundry Work timeline component placeholder.
+ *
+ * FE-01 skeleton only. UI implementation belongs to FE-04 UI Composition.
+ */
+
+export function LaundryWorkTimeline() {
+  return null
+}
