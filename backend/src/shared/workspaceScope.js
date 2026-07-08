07510ab253a110c8d1348714f37e1ad56071d9b2
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 14:01:53 +0700

    BE-07 re-export workspace scope from policy boundary

--- backend/src/shared/workspaceScope.js
+++ backend/src/shared/workspaceScope.js
@@ -1,22 +1,6 @@
-const buildResortScopedWhere = ({ workspaceType, resortId } = {}) => {
-  const where = {};
-
-  if (workspaceType === 'RESORT') {
-    if (!resortId) {
-      const error = new Error('resortId is required for Resort Workspace requests');
-      error.statusCode = 400;
-      throw error;
-    }
-
-    where.resortId = Number(resortId);
-  }
-
-  if (workspaceType !== 'RESORT' && resortId) {
-    where.resortId = Number(resortId);
-  }
-
-  return where;
-};
+const {
+  buildResortScopedWhere,
+} = require('../policies/workspace.policy');
 
 module.exports = {
   buildResortScopedWhere,
