dbb9c29812ff140650c05fba8aa73b7ba2aa544e
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 07:31:33 +0700

    BE-04.02 wire laundry bags routes to controller

--- backend/src/routes/laundryBags.routes.js
+++ backend/src/routes/laundryBags.routes.js
@@ -1,48 +1,15 @@
 const express = require('express');
 
-const { sendSuccess } = require('../core/httpResponse');
 const {
-  listLaundryBags,
-  getLaundryBagById,
-  createLaundryBag,
-} = require('../services/laundryBags.service');
-const {
-  parseRequest,
-  listLaundryBagsQuerySchema,
-  getLaundryBagQuerySchema,
-  createLaundryBagBodySchema,
-} = require('../validators/laundryBags.validator');
+  listLaundryBagsController,
+  getLaundryBagController,
+  createLaundryBagController,
+} = require('../controllers/laundryBags.controller');
 
 const router = express.Router({ mergeParams: true });
 
-router.get('/', async (req, res, next) => {
-  try {
-    const query = parseRequest(listLaundryBagsQuerySchema, req.query);
-    const result = await listLaundryBags(req.params.workId, query);
-    return sendSuccess(res, result.items, { pagination: result.pagination });
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.get('/:bagId', async (req, res, next) => {
-  try {
-    const query = parseRequest(getLaundryBagQuerySchema, req.query);
-    const bag = await getLaundryBagById(req.params.workId, req.params.bagId, query);
-    return sendSuccess(res, bag);
-  } catch (error) {
-    return next(error);
-  }
-});
-
-router.post('/', async (req, res, next) => {
-  try {
-    const body = parseRequest(createLaundryBagBodySchema, req.body);
-    const bag = await createLaundryBag(req.params.workId, body);
-    return sendSuccess(res, bag, undefined, 201);
-  } catch (error) {
-    return next(error);
-  }
-});
+router.get('/', listLaundryBagsController);
+router.get('/:bagId', getLaundryBagController);
+router.post('/', createLaundryBagController);
 
 module.exports = router;
