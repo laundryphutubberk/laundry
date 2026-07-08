26ef313b97b85db1824084c42d7dcb97b3d69f3a
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 15:03:08 +0700

    BE-07 add authorization policy helpers

---
+++ backend/src/policies/authorization.policy.js
@@ -0,0 +1,57 @@
+const { USER_ROLES, WORKSPACE_TYPES, assertValidActor } = require('../core/actor');
+
+const createAuthorizationError = (message, statusCode = 403) => {
+  const error = new Error(message);
+  error.statusCode = statusCode;
+  error.code = 'AUTHORIZATION_POLICY_VIOLATION';
+  return error;
+};
+
+const LAUNDRY_MANAGEMENT_ROLES = Object.freeze([
+  USER_ROLES.LAUNDRY_OWNER,
+  USER_ROLES.LAUNDRY_MANAGER,
+]);
+
+const LAUNDRY_STAFF_ROLES = Object.freeze([
+  USER_ROLES.LAUNDRY_OWNER,
+  USER_ROLES.LAUNDRY_MANAGER,
+  USER_ROLES.LAUNDRY_STAFF,
+]);
+
+const assertLaundryWorkspaceActor = (actor) => {
+  const validActor = assertValidActor(actor);
+
+  if (validActor.workspaceType !== WORKSPACE_TYPES.LAUNDRY) {
+    throw createAuthorizationError('Laundry workspace access is required');
+  }
+
+  return validActor;
+};
+
+const assertLaundryManagementActor = (actor) => {
+  const validActor = assertLaundryWorkspaceActor(actor);
+
+  if (!LAUNDRY_MANAGEMENT_ROLES.includes(validActor.role)) {
+    throw createAuthorizationError('Laundry management permission is required');
+  }
+
+  return validActor;
+};
+
+const assertLaundryStaffActor = (actor) => {
+  const validActor = assertLaundryWorkspaceActor(actor);
+
+  if (!LAUNDRY_STAFF_ROLES.includes(validActor.role)) {
+    throw createAuthorizationError('Laundry staff permission is required');
+  }
+
+  return validActor;
+};
+
+module.exports = {
+  LAUNDRY_MANAGEMENT_ROLES,
+  LAUNDRY_STAFF_ROLES,
+  assertLaundryWorkspaceActor,
+  assertLaundryManagementActor,
+  assertLaundryStaffActor,
+};
