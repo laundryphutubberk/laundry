9b0e558a8a77d202e2eeb7ad5aa7fb7834505b9f
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 10:54:32 +0700

    BE-MIGRATION: add laundry bag controller module

---
+++ backend/src/modules/laundry-bag/laundry-bag.controller.js
@@ -0,0 +1,48 @@
+const { sendSuccess } = require('../../core/httpResponse');
+const {
+  listLaundryBags,
+  getLaundryBagById,
+  createLaundryBag,
+} = require('./laundry-bag.service');
+const {
+  parseRequest,
+  listLaundryBagsQuerySchema,
+  getLaundryBagQuerySchema,
+  createLaundryBagBodySchema,
+} = require('./laundry-bag.validation');
+
+const listLaundryBagsController = async (req, res, next) => {
+  try {
+    const query = parseRequest(listLaundryBagsQuerySchema, req.query);
+    const result = await listLaundryBags(req.params.workId, query);
+    return sendSuccess(res, result.items, { pagination: result.pagination });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const getLaundryBagController = async (req, res, next) => {
+  try {
+    const query = parseRequest(getLaundryBagQuerySchema, req.query);
+    const bag = await getLaundryBagById(req.params.workId, req.params.bagId, query);
+    return sendSuccess(res, bag);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const createLaundryBagController = async (req, res, next) => {
+  try {
+    const body = parseRequest(createLaundryBagBodySchema, req.body);
+    const bag = await createLaundryBag(req.params.workId, body);
+    return sendSuccess(res, bag, undefined, 201);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = {
+  listLaundryBagsController,
+  getLaundryBagController,
+  createLaundryBagController,
+};
