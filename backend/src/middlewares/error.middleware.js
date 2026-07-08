88876f84d65d17965491576b880e5dc6fdb95ec6
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 14:39:21 +0700

    BE-07 expose safe error codes

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,9 +1,23 @@
 const { sendFailure } = require('../core/httpResponse');
 
+const buildErrorMeta = (error) => {
+  const meta = {};
+
+  if (error.code) {
+    meta.code = error.code;
+  }
+
+  if (error.details) {
+    meta.details = error.details;
+  }
+
+  return Object.keys(meta).length > 0 ? meta : undefined;
+};
+
 const errorMiddleware = (error, _req, res, _next) => {
   const statusCode = error.statusCode || 500;
   const message = statusCode === 500 ? 'Internal server error' : error.message;
-  const meta = error.details ? { details: error.details } : undefined;
+  const meta = statusCode === 500 ? undefined : buildErrorMeta(error);
 
   return sendFailure(res, { message }, statusCode, meta);
 };
