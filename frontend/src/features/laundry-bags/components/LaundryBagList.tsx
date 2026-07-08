04a69b20b106a6fbf1011c0c6ea9db69f6ff6f0d
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 21:14:11 +0700

    FE-01: add laundry bag list placeholder

---
+++ frontend/src/features/laundry-bags/components/LaundryBagList.tsx
@@ -0,0 +1,9 @@
+/**
+ * Laundry Bag list component placeholder.
+ *
+ * FE-01 skeleton only. UI implementation belongs to FE-04 UI Composition.
+ */
+
+export function LaundryBagList() {
+  return null
+}
