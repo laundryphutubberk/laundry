f3167b6e61c4cfdbe04da8276584141c02a29cc3
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 21:15:01 +0700

    FE-01: add laundry bag page placeholder

---
+++ frontend/src/features/laundry-bags/pages/LaundryBagPage.tsx
@@ -0,0 +1,9 @@
+/**
+ * Laundry Bag route page placeholder.
+ *
+ * FE-01 skeleton only. Prefer bag panels inside Laundry Work Detail unless standalone page is justified.
+ */
+
+export function LaundryBagPage() {
+  return null
+}
