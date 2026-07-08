411928c2ef9d4ed12747557d2338eeb18f22681e
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 21:12:33 +0700

    FE-01: add laundry work controller hook placeholder

---
+++ frontend/src/features/laundry-works/hooks/useLaundryWorkController.ts
@@ -0,0 +1,9 @@
+/**
+ * Laundry Work controller hook placeholder.
+ *
+ * FE-01 skeleton only. Runtime orchestration belongs here when implemented.
+ */
+
+export function useLaundryWorkController() {
+  return {}
+}
