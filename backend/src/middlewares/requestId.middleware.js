02ce0b229fce7eba44499fce8aab3133041d6458
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 19:43:01 +0700

    BE-09: add correlation id propagation

--- backend/src/middlewares/requestId.middleware.js
+++ backend/src/middlewares/requestId.middleware.js
@@ -1,10 +1,22 @@
 const crypto = require('crypto');
 
+const getHeaderValue = (value) => {
+  if (Array.isArray(value)) {
+    return value[0];
+  }
+
+  return value;
+};
+
 const requestIdMiddleware = (req, res, next) => {
-  const requestId = req.headers['x-request-id'] || crypto.randomUUID();
+  const requestId = getHeaderValue(req.headers['x-request-id']) || crypto.randomUUID();
+  const correlationId = getHeaderValue(req.headers['x-correlation-id']) || requestId;
 
   req.requestId = requestId;
+  req.correlationId = correlationId;
+
   res.setHeader('x-request-id', requestId);
+  res.setHeader('x-correlation-id', correlationId);
 
   next();
 };
