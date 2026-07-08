e128309aa26dacb497d11042292b2839dbcbd791
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-08 17:44:06 +0700

    BE-07 protect feature-first laundry routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { getHealthStatus } = require('../core/health');
 const { sendSuccess } = require('../core/httpResponse');
+const { authActorMiddleware } = require('../middlewares/authActor.middleware');
 const laundryWorksRoutes = require('../modules/laundry-work');
 const laundryBagsRoutes = require('../modules/laundry-bag');
 
@@ -16,7 +17,7 @@ router.get('/health', async function healthHandler(_req, res, next) {
   }
 });
 
-router.use('/laundry/works', laundryWorksRoutes);
-router.use('/laundry/works/:workId/bags', laundryBagsRoutes);
+router.use('/laundry/works/:workId/bags', authActorMiddleware, laundryBagsRoutes);
+router.use('/laundry/works', authActorMiddleware, laundryWorksRoutes);
 
 module.exports = router;
