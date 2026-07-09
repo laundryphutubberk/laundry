398d3c2dd4b9145d765f24405c14c28cd78b51f1
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-09 22:09:29 +0700

    feat(auth): add register controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 const authService = require('../services/auth.service');
 const { sendSuccess } = require('../core/httpResponse');
-const { validateLoginInput } = require('../validators/auth.validator');
+const { validateLoginInput, validateRegisterInput } = require('../validators/auth.validator');
 
 const login = async (req, res, next) => {
   try {
@@ -13,6 +13,18 @@ const login = async (req, res, next) => {
   }
 };
 
+const register = async (req, res, next) => {
+  try {
+    const input = validateRegisterInput(req.body);
+    const session = await authService.register(input);
+
+    return sendSuccess(res, session, undefined, 201);
+  } catch (error) {
+    return next(error);
+  }
+};
+
 module.exports = {
   login,
+  register,
 };
