34375a3d12c1344a3c792cce97ad67ca4aac17a0
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-10 15:12:35 +0700

    feat(laundry): expose laundry issue routes

--- backend/src/routes/laundryWorks.routes.js
+++ backend/src/routes/laundryWorks.routes.js
@@ -7,12 +7,22 @@ const {
   updateLaundryWorkStatusController,
   deleteLaundryWorkController,
 } = require('../controllers/laundryWorks.controller');
+const {
+  listLaundryIssuesController,
+  createLaundryIssueController,
+  updateLaundryIssueController,
+  resolveLaundryIssueController,
+} = require('../controllers/laundryIssues.controller');
 
 const router = express.Router();
 
 router.get('/', listLaundryWorksController);
-router.get('/:workId', getLaundryWorkController);
 router.post('/', createLaundryWorkController);
+router.get('/:workId/issues', listLaundryIssuesController);
+router.post('/:workId/issues', createLaundryIssueController);
+router.patch('/issues/:issueId', updateLaundryIssueController);
+router.patch('/issues/:issueId/resolve', resolveLaundryIssueController);
+router.get('/:workId', getLaundryWorkController);
 router.patch('/:workId/status', updateLaundryWorkStatusController);
 router.delete('/:workId', deleteLaundryWorkController);
 
