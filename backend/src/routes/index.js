56585d240b0d7e501fb2e7f192fd75d8e8f4958e
laundryphutubberk <laundry.phutubberk@gmail.com>
2026-07-10 14:43:19 +0700

    feat(laundry): mount issue endpoints

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -8,10 +8,15 @@ const resortsRoutes = require('./resorts.routes');
 const laundryWorksRoutes = require('./laundryWorks.routes');
 const laundryBagsRoutes = require('./laundryBags.routes');
 const laundryCountLinesRoutes = require('./laundryCountLines.routes');
+const laundryIssuesRoutes = require('./laundryIssues.routes');
 const {
   updateLaundryCountLineController,
   deleteLaundryCountLineController,
 } = require('../controllers/laundryCountLines.controller');
+const {
+  updateLaundryIssueController,
+  resolveLaundryIssueController,
+} = require('../controllers/laundryIssues.controller');
 
 const router = express.Router();
 
@@ -28,8 +33,11 @@ router.use('/auth', authRoutes);
 router.use('/resorts', authActorMiddleware, resortsRoutes);
 router.use('/laundry/works/:workId/bags', authActorMiddleware, laundryBagsRoutes);
 router.use('/laundry/works/:workId/count-lines', authActorMiddleware, laundryCountLinesRoutes);
+router.use('/laundry/works/:workId/issues', authActorMiddleware, laundryIssuesRoutes);
 router.patch('/laundry/count-lines/:lineId', authActorMiddleware, updateLaundryCountLineController);
 router.delete('/laundry/count-lines/:lineId', authActorMiddleware, deleteLaundryCountLineController);
+router.patch('/laundry/issues/:issueId', authActorMiddleware, updateLaundryIssueController);
+router.patch('/laundry/issues/:issueId/resolve', authActorMiddleware, resolveLaundryIssueController);
 router.use('/laundry/works', authActorMiddleware, laundryWorksRoutes);
 
 module.exports = router;
